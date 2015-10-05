0541940a77682906b4f19ab6e1d43d059cf2ae59
Vit <murvit@ukr.net>
2015-10-05 12:26:33 +0300

    add users

--- src/main/java/com/vmurashkin/tradermvc/entities/User.java
+++ src/main/java/com/vmurashkin/tradermvc/entities/User.java
@@ -44,14 +44,6 @@ public class User {
         return name;
     }
 
-//    public String getRole() {
-//        return role;
-//    }
-//
-//    public void setRole(String role) {
-//        this.role = role;
-//    }
-
     public String getPassword() {
         return password;
     }
