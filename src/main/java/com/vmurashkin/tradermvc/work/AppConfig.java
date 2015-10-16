8324c6df31a64f12f3954ba1a651104ae0c84c67
Vit <murvit@ukr.net>
2015-10-16 17:20:32 +0300

    make JPA config

--- src/main/java/com/vmurashkin/tradermvc/work/AppConfig.java
+++ src/main/java/com/vmurashkin/tradermvc/work/AppConfig.java
@@ -4,19 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
-
-import javax.persistence.EntityManagerFactory;
-import java.util.Properties;
 
 /**
  * Application configuration
@@ -25,11 +15,8 @@ import java.util.Properties;
 @Configuration
 @ComponentScan("com.vmurashkin.tradermvc")
 @EnableWebMvc
-@EnableTransactionManagement
-@Import({AppSecurityConfig.class})
+@Import({AppSecurityConfig.class, JPAConfig.class})
 public class AppConfig {
-
-//    private static EntityManagerFactory EMFinstance;
 
     @Bean
     public UrlBasedViewResolver setupViewResolver() {
@@ -41,60 +28,11 @@ public class AppConfig {
         return resolver;
     }
 
-    @Bean//(name = "dataSource")
-    public DriverManagerDataSource dataSource() {
-        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-
-//        driverManagerDataSource.setUrl("jdbc:mysql://mysql22751-trader.unicloud.pl:3306/trader?characterEncoding=UTF-8");
-//        driverManagerDataSource.setUsername("root");
-//        driverManagerDataSource.setPassword("IOSfmx55982");
-
-
-        driverManagerDataSource.setUrl("jdbc:mysql://localhost/trader?characterEncoding=UTF-8");
-        driverManagerDataSource.setUsername("testuser");
-        driverManagerDataSource.setPassword("testpassword");
-
-        return driverManagerDataSource;
-    }
-
     @Bean
     public TraderDAO traderDAO() {
         return new TraderDAOImpl();
     }
 
-//    @Bean
-//    public static EntityManager getEMinstance() {
-//        if (EMFinstance == null)
-//            EMFinstance = Persistence.createEntityManagerFactory("trader");
-//        return EMFinstance.createEntityManager();
-//    }
 
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource());
-        em.setPackagesToScan(new String[] { "com.vmurashkin.tradermvc" });
-        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-        em.setJpaProperties(additionalProperties());
-        return em;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(emf);
-        return transactionManager;
-    }
-
-    Properties additionalProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "update");
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-        properties.setProperty("hibernate.show_sql", "true");
-        properties.setProperty("hibernate.format_sql", "true");
-        return properties;
-    }
 
 }
---
+++ src/main/java/com/vmurashkin/tradermvc/work/JPAConfig.java
@@ -0,0 +1,67 @@
+package com.vmurashkin.tradermvc.work;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.Properties;
+
+/**
+ * JPA / Hibernate configuration
+ */
+
+@Configuration
+@EnableTransactionManagement
+public class JPAConfig {
+
+    @Bean//(name = "dataSource")
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+
+//        driverManagerDataSource.setUrl("jdbc:mysql://mysql22751-trader.unicloud.pl:3306/trader?characterEncoding=UTF-8");
+//        driverManagerDataSource.setUsername("root");
+//        driverManagerDataSource.setPassword("IOSfmx55982");
+
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost/trader?characterEncoding=UTF-8");
+        driverManagerDataSource.setUsername("testuser");
+        driverManagerDataSource.setPassword("testpassword");
+
+        return driverManagerDataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(new String[] { "com.vmurashkin.tradermvc" });
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+        return transactionManager;
+    }
+
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.format_sql", "true");
+        return properties;
+    }
+
+}
